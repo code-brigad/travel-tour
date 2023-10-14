fb5bdc53e1a11c23e1396c1ef63b37e93ec4a2d0
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-10-14 15:52:24 +0500

    Navbar

---
+++ components/ChangeLang.jsx
@@ -0,0 +1,115 @@
+import { AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React from "react";
+import { useTranslation, motion } from "react-i18next";
+
+const lng = [
+  {
+    id: 1,
+    shortcut: "uz",
+    name: "O`zbek tili",
+  },
+  {
+    id: 2,
+    shortcut: "ru",
+    name: "Русский",
+  },
+];
+
+export const ChangeLang = ({ open, setOpen }) => {
+  const { i18n } = useTranslation();
+  const router = useRouter();
+  const { pathname, asPath, query } = router;
+
+  const handleSetLng = (lng) => {
+    router.push({ pathname, query }, asPath, {
+      locale: lng,
+    });
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <AnimatePresence>
+        {open && (
+          <>
+            <motion.nav
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 20, opacity: 0 }}
+              transition={{
+                stiffness: 500,
+                duration: 0.2,
+              }}
+              className="absolute left-[-88px] top-[50px] text-black bg-white py-4 rounded-lg shadow-lg   before:absolute before:block before:w-[14px] before:h-[14px] before:rotate-45 before:bg-white before:top-[-7px] before:left-[50%] before:translate-x-[-50%] before:z-[-1]"
+            >
+              <ul className="flex flex-col">
+                {lng.map((data) => {
+                  return (
+                    <li
+                      key={data.id}
+                      className={`flex flex-row items-center justify-between min-w-[210px] gap-4 transition-colors duration-200 hover:bg-main/10 px-3 py-2 cursor-pointer ${
+                        i18n.language == data.shortcut ? "text-main" : ""
+                      }`}
+                      onClick={() => handleSetLng(data.shortcut)}
+                    >
+                      <div className="flex flex-row gap-4">
+                        <p className="uppercase">{data.shortcut}</p>
+                        <p>{data.name}</p>
+                      </div>
+                      {i18n.language == data.shortcut ? <IconCheck /> : ""}
+                    </li>
+                  );
+                })}
+              </ul>
+            </motion.nav>
+          </>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+const flag = [
+  {
+    id: 1,
+    img: "/images/uzbekistan.png",
+    value: "uz",
+  },
+  {
+    id: 2,
+    img: "/images/russia.png",
+    value: "ru",
+  },
+];
+
+export const ChangeLangMobile = () => {
+  const { i18n } = useTranslation('common')
+  const router = useRouter();
+  const { pathname, asPath, query } = router;
+  const handleSetLng = (lng) => {
+    router.push({ pathname, query }, asPath, {
+      locale: lng,
+    });
+  };
+  return (
+    <div className="flex w-full justify-start items-center">
+      <ul className="flex flex-row gap-2 h-fit">
+        {flag.map((flag) => {
+          return (
+            <li key={flag.id} onClick={() => handleSetLng(flag.value)}>
+              <Image
+                src={flag.img}
+                alt={flag.value}
+                width={200}
+                height={200}
+                className={`w-[35px] h-[35px] rounded-full cursor-pointer ${i18n.language == flag.value ? 'border-2' : ''}`}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
--- sections/Navbar.jsx
+++ sections/Navbar.jsx
@@ -5,14 +5,13 @@ import { useState } from "react";
 import { useTranslation } from "next-i18next";
 import {
   IconArrowBottom,
-  IconCheck,
   IconClose,
   IconHelp,
   IconLang,
   IconMenu,
   IconPlane,
 } from "@/icons";
-import { useRouter } from "next/router";
+import { ChangeLang, ChangeLangMobile } from "@/components/ChangeLang";
 
 const links = [
   {
@@ -57,7 +56,7 @@ const Navbar = () => {
               </li>
             );
           })}
-          <li className="relative">
+          <li className="relative sm:block hidden">
             <div
               className={`hover:bg-white/10 rounded-[5px] p-2 cursor-pointer transition-colors duration-500 flex flex-row gap-1 ${
                 open == true ? "bg-white/10" : ""
@@ -82,7 +81,7 @@ const Navbar = () => {
             <IconMenu />
           </li>
         </ul>
-        <Mobile menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Mobile menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </motion.nav>
   );
@@ -91,99 +90,52 @@ const Navbar = () => {
 export default Navbar;
 
 export const Mobile = ({ menuOpen, setMenuOpen }) => {
+  const { t } = useTranslation("common");
   return (
     <AnimatePresence>
       {menuOpen && (
-        <motion.nav
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
-          transition={{
-            stiffness: 500,
-            duration: 0.2,
-          }}
-          className="custom-container sm:hidden block absolute top-0 left-0 z-10 bg-main w-full h-screen py-4"
-        >
-          <div className="flex flex-row items-center justify-between">
-            <Link href={"/"} className={`${young_serif.className}`}>
-              Sayohat
-            </Link>
-            <div
-              className="hover:bg-white/10 rounded-[5px] p-2 cursor-pointer transition-colors duration-500"
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              <IconClose />
+        <>
+          <div
+            className="w-full h-screen bg-[#172554]/[0.5] md:hidden block absolute top-0 left-0"
+            onClick={() => setMenuOpen(!menuOpen)}
+          ></div>
+          <motion.nav
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{
+              stiffness: 500,
+              duration: 0.2,
+            }}
+            className="sm:hidden block absolute top-0 left-0 z-10 bg-main max-w-[330px] w-full h-screen py-6"
+          >
+            <div className="px-4 flex flex-row items-center justify-between mb-6">
+              <ChangeLangMobile />
+              <div
+                className="hover:bg-white/10 rounded-[5px] p-2 cursor-pointer transition-colors duration-500"
+                onClick={() => setMenuOpen(!menuOpen)}
+              >
+                <IconClose />
+              </div>
             </div>
-          </div>
-        </motion.nav>
+            <ul>
+              {links.map((link) => {
+                return (
+                  <li className="hover:bg-white/10 px-4">
+                    <Link
+                      className="flex flex-row items-center gap-2 py-2"
+                      href={link.link}
+                    >
+                      <link.icon />
+                      <p>{t(link.name)}</p>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.nav>
+        </>
       )}
     </AnimatePresence>
-  );
-};
-
-export const ChangeLang = ({ open, setOpen }) => {
-  const { i18n } = useTranslation();
-  const router = useRouter();
-  const { pathname, asPath, query, locale } = router;
-
-  const handleSetLng = (lng) => {
-    router.push({ pathname, query }, asPath, {
-      locale: lng,
-    });
-    setOpen(!open);
-  };
-
-  const lng = [
-    {
-      id: 1,
-      shortcut: "uz",
-      name: "O`zbek tili",
-    },
-    {
-      id: 2,
-      shortcut: "ru",
-      name: "Русский",
-    },
-  ];
-
-  return (
-    <>
-      <AnimatePresence>
-        {open && (
-          <>
-            <motion.nav
-              initial={{ y: 20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: 20, opacity: 0 }}
-              transition={{
-                stiffness: 500,
-                duration: 0.2,
-              }}
-              className="absolute left-[-88px] top-[50px] text-black bg-white py-4 rounded-lg shadow-lg   before:absolute before:block before:w-[14px] before:h-[14px] before:rotate-45 before:bg-white before:top-[-7px] before:left-[50%] before:translate-x-[-50%] before:z-[-1]"
-            >
-              <ul className="flex flex-col">
-                {lng.map((data) => {
-                  return (
-                    <li
-                      key={data.id}
-                      className={`flex flex-row items-center justify-between min-w-[210px] gap-4 transition-colors duration-200 hover:bg-main/10 px-3 py-2 cursor-pointer ${
-                        i18n.language == data.shortcut ? "text-main" : ""
-                      }`}
-                      onClick={() => handleSetLng(data.shortcut)}
-                    >
-                      <div className="flex flex-row gap-4">
-                        <p className="uppercase">{data.shortcut}</p>
-                        <p>{data.name}</p>
-                      </div>
-                      {i18n.language == data.shortcut ? <IconCheck /> : ""}
-                    </li>
-                  );
-                })}
-              </ul>
-            </motion.nav>
-          </>
-        )}
-      </AnimatePresence>
-    </>
   );
 };
