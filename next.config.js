dc7d298789adcb6cd322d021f9a1f9a17efc2da0
usmonovdev <usmonovazizbek045@gmail.com>
2023-10-23 21:19:52 +0500

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  presets: ['next/babel'],
+  plugins: [['@babel/plugin-transform-runtime', { regenerator: true }]],
 }
 
 module.exports = nextConfig
