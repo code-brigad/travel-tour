7332b574f1d0d55d2acf3de4746f680e3b7d594f
usmonovdev <usmonovazizbek045@gmail.com>
2023-10-24 00:01:52 +0500

    next.config file update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   reactStrictMode: true,
-  i18n
+  i18n,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      'core-js/modules/es.array.map': require.resolve('core-js/modules/es.array.map'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
