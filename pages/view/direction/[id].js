e83ac2bb9b055203e1bfd11c7aa9303c351c7e53
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-10-23 18:14:05 +0500

    vercel id

--- pages/view/direction/[id].js
+++ pages/view/direction/[id].js
@@ -28,7 +28,7 @@ const Open = () => {
   )
 }
 
-export async function getServerSideProps({ locale }) {
+export async function getStaticProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, [
