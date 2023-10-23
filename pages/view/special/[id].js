1761164ba0b7efe3cda7a5e84cdb844ef3894b00
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-10-23 17:30:55 +0500

    vercel bug fix

--- pages/view/special/[id].js
+++ pages/view/special/[id].js
@@ -2,65 +2,106 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { TextSubtitle } from '@/theme/Text';
 import { useRouter } from 'next/router';
+import { IconMoney, IconTelegram } from '@/icons';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Error, Loading } from '@/components';
+import { replaceWithLocale } from '@/layouts/replaceWithLocale';
 import Head from 'next/head';
 import Image from 'next/image';
 import Plane from '@/icons/Plane';
-import getWindowSize from '@/layouts/getWindowSize';
-import { IconMoney } from '@/icons';
+import axios from "@/config/axios.config"
+import Link from 'next/link';
 
 const Open = () => {
   const { t, i18n } = useTranslation('common')
   const router = useRouter()
-  const { screenSize } = getWindowSize()
+  const [tourPackage, setTourPackage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const getPackages = async () => {
+    setIsLoading(true);
+    setIsError(false);
+    try {
+      const { data } = await axios.get(`travel/${router.query.id}`);
+      setTourPackage(data);
+      setIsLoading(false);
+      setIsError(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getPackages();
+  }, [router]);
+
+  if (isLoading) {
+    return (
+      <div className='custom-container py-[150px]'>
+        <Loading />
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div className='custom-container py-[150px]'>
+        <Error />
+      </div>
+    )
+  }
 
   return (
     <>
       <Head>
-        <title>{router.query.whereFrom.toUpperCase()}	&rarr; {router.query.where.toUpperCase()}</title>
+        <title>{tourPackage[replaceWithLocale(router, "from_")]}	&rarr; {tourPackage[replaceWithLocale(router, "where_")]}</title>
       </Head>
-      <section className='sm:pt-[150px] pt-[100px] pb-[150px] flex flex-col gap-6 custom-container'>
+      <section className='sm:pt-[150px] pt-[100px] pb-[50px] flex flex-col gap-6 custom-container'>
         <div className='w-full'>
-          {screenSize.width > 640 ?
-            <Image src={'/images/big-placeholder.png'} alt='placeholder' width={500} height={500} className='w-full rounded-[20px]' />
-            :
-            <Image src={'/images/placeholder.png'} alt='placeholder' width={500} height={500} className='w-full rounded-[20px]' />
-          }
+          <Image src={'/images/big-placeholder.png'} alt='placeholder' width={500} height={500} className='w-full rounded-[20px]' />
         </div>
         <div className='sm:w-fit w-full rounded-[5px] text-white flex sm:flex-row flex-col sm:items-center items-start sm:gap-4 gap-2'>
           <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main px-4 py-4 rounded-[10px]'>
             <Plane fatherClass={'fill-white rotate-[50deg]'} />
-            <TextSubtitle className={"!text-white capitalize"}>Qayerdan: <span className='font-semibold'>{router.query.whereFrom}</span></TextSubtitle>
+            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.from")}: <span className='font-semibold'>{tourPackage[replaceWithLocale(router, "from_")]}</span></TextSubtitle>
           </div>
           <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main px-4 py-4 rounded-[10px]'>
             <Plane fatherClass={'fill-white rotate-[130deg]'} />
-            <TextSubtitle className={"!text-white capitalize"}>Qayerga: <span className='font-semibold'>{router.query.where}</span></TextSubtitle>
+            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.where")}: <span className='font-semibold'>{tourPackage[replaceWithLocale(router, "where_")]}</span></TextSubtitle>
           </div>
           <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main px-4 py-4 rounded-[10px]'>
             <IconMoney fatherClass={''} />
-            <TextSubtitle className={"!text-white capitalize"}>Narx: <span className='font-semibold'>15.000.00 so'm</span></TextSubtitle>
+            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.price")}: <span className='font-semibold'>{tourPackage.price} so&apos;m</span></TextSubtitle>
           </div>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque ut expedita repellendus eligendi neque deleniti suscipit, et quibusdam dolor quidem, aperiam assumenda nesciunt voluptate obcaecati. Alias, quasi quia. Molestiae provident nisi nihil? Fugit, quisquam. A dolorem doloribus obcaecati natus, laudantium aliquid, officiis, recusandae eum perspiciatis in sint. Architecto labore assumenda laborum tempore, harum et veritatis est unde eligendi. Perferendis iure omnis odit velit? Magni saepe quos adipisci, at consequuntur nisi soluta nulla, distinctio cum amet voluptatem accusantium veniam. Illum asperiores distinctio ab dolorem corrupti dignissimos obcaecati. Praesentium commodi maxime, eaque veniam sit eos sapiente beatae quia omnis modi impedit laborum libero recusandae consequatur, culpa dolorem. Dolores nostrum repellendus impedit fuga aliquam excepturi praesentium, eveniet, ullam molestias optio unde. Qui.
-          </TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{tourPackage[replaceWithLocale(router, "description_")]}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>Qayerdan: Toshkent</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.from")}: {tourPackage[replaceWithLocale(router, "from_")]}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>Qayerga: Jidda</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.where")}: {tourPackage[replaceWithLocale(router, "where_")]}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>Sayohat davomiyligi: 14 kun / 13 tun</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.days")}: {tourPackage.days
+          } {t("openpackage.day")} / {tourPackage.days - 1} {t("openpackage.night")}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>Narx: 15.000.00</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.fly")}: {tourPackage.fly_date}</TextSubtitle>
         </div>
-        <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main/[0.03] p-4 border rounded-[10px]'>
-          <Plane fatherClass={'fill-black rotate-[50deg]'} />
-          <TextSubtitle className={"!text-black capitalize"}>Menejer bilan bog'lanish</TextSubtitle>
+        <div className='bg-main p-4 border rounded-[10px] flex flex-col gap-2'>
+          <TextSubtitle className={'text-start !text-white'}>{t("openpackage.price")}: {tourPackage.price} So&apos;m</TextSubtitle>
         </div>
+        <Link href={'/'}>
+          <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main/[0.03] p-4 border border-main rounded-[10px]'>
+            <IconTelegram childClass={'!stroke-main'} />
+            <TextSubtitle className={"!text-black capitalize"}>{t('openpackage.manager')}</TextSubtitle>
+          </div>
+        </Link>
       </section>
     </>
   )
