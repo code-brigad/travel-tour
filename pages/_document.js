8150350a254fa92de8d97e65b9dcc0d8b8621645
usmonovdev <usmonovazizbek045@gmail.com>
2023-10-24 21:55:23 +0500

    seo images added

--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,31 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head>
-        <title>Travel agency</title>
-        <meta name="author" content='code-brigade.uz'/>
-        <meta name='description' content='Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане.'/>
+        <title>Travel House 777</title>
+        <meta name="author" content='code-brigade.uz' />
+        <meta name='description' content='Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане.' />
         <meta name="keywords" content="отдых цена, тур цена, туры в малайзию, туры в малайзию из ташкента, туры в дубай из ташкента, горящие туры из ташкента, туры в таиланд из ташкента, тур по европе, туры в израиль из ташкента, туры в чехию из ташкента, туры мальдивы из ташкента, морские круизы, тур франция, тур португалия, тур италия, индивидуальные туры, италия туры 2023, туры из ташкента 2023" />
-        
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" href="/images/icons/32.png" sizes='any' />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/images/icons/192.png" />
+        <meta property="og:title" content="Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане." />
+        <meta property="og:description" content="Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане."
+        />
+        <meta name="title" content="Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане." />
+        <meta name="author" content="https://tour-house.uz/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Code Brigade - Dasturchilar Brigadasi" />
+        <meta property="og:url" content="https://tour-house.uz/" />
+        <meta property="og:title" content="Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане." />
+        <meta property="og:description" content="Travel House – национальный туроператор, специализирующийся на международном выездном, въездном и внутреннем туризме в Узбекистане." />
+        <meta property="og:image" content="/images/icons/192.png" />
+        <meta property="og:image:width" content="500" />
+        <meta property="og:image:height" content="500" />
       </Head>
       <body>
         <Main />
