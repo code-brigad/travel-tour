6ca0668fd20e587213cd53e4e7c7249f7cd7166a
usmonovdev <usmonovazizbek045@gmail.com>
2023-10-24 13:29:35 +0500

    update id

--- pages/view/direction/[id].js
+++ pages/view/direction/[id].js
@@ -1,78 +1,78 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-// import { useTranslation } from 'next-i18next'
+import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router';
-// import { TextSubtitle } from '@/theme/Text';
-// import { IconMoney, IconTelegram } from '@/icons';
-// import { useState } from 'react';
-// import { useEffect } from 'react';
-// import { replaceWithLocale } from "@/layouts/replaceWithLocale"
-// import Head from 'next/head';
-// import Image from 'next/image';
-// import Plane from '@/icons/Plane';
-// import axios from "axios"
-// import Link from 'next/link';
-// import { Error, Loading } from '@/components';
+import { TextSubtitle } from '@/theme/Text';
+import { IconMoney, IconTelegram } from '@/icons';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { replaceWithLocale } from "@/layouts/replaceWithLocale"
+import Head from 'next/head';
+import Image from 'next/image';
+import Plane from '@/icons/Plane';
+import axios from "axios"
+import Link from 'next/link';
+import { Error, Loading } from '@/components';
 
 const OpenDirection = () => {
   // console.log(tourPackage, "user");
-  // const { t, i18n } = useTranslation('common')
+  const { t, i18n } = useTranslation('common')
   const router = useRouter()
   console.log(router);
-  // // const [tourPackage, setTourPackage] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [isError, setIsError] = useState(false);
+  const [tourPackage, setTourPackage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  // const getPackages = async () => {
-  //   setIsLoading(true);
-  //   setIsError(false);
-  //   try {
-  //     const { data } = await axios.get(`https://tour-spsn.onrender.com/api/travel/${router.query.id}`);
-  //     setTourPackage(data);
-  //     setIsLoading(false);
-  //     setIsError(false);
-  //   } catch (error) {
-  //     setIsLoading(false);
-  //     setIsError(true);
-  //   }
-  // };
+  const getPackages = async () => {
+    setIsLoading(true);
+    setIsError(false);
+    try {
+      const { data } = await axios.get(`https://tour-spsn.onrender.com/api/travel/${router.query.id}`);
+      setTourPackage(data);
+      setIsLoading(false);
+      setIsError(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
 
-  // useEffect(() => {
-  //   getPackages();
-  // }, [router]);
+  useEffect(() => {
+    getPackages();
+  }, [router]);
 
-  // if (isLoading) {
-  //   return (
-  //     <div className='custom-container py-[150px]'>
-  //       <Loading />
-  //     </div>
-  //   )
-  // }
+  if (isLoading) {
+    return (
+      <div className='custom-container py-[150px]'>
+        <Loading />
+      </div>
+    )
+  }
 
-  // if (isError) {
-  //   return (
-  //     <div className='custom-container py-[150px]'>
-  //       <Error />
-  //     </div>
-  //   )
-  // }
+  if (isError) {
+    return (
+      <div className='custom-container py-[150px]'>
+        <Error />
+      </div>
+    )
+  }
 
   return (
     <>
-      {/* <Head>
-        <title>{tourPackage[replaceWithLocale(router, "from_")]}	&rarr; {tourPackage[replaceWithLocale(router, "where_")]}</title>
-      </Head> */}
-      {/* <section className='sm:pt-[150px] pt-[100px] pb-[50px] flex flex-col gap-6 custom-container'>
+      <Head>
+        <title>{tourPackage.from_uz}	&rarr; {tourPackage.where_uz}</title>
+      </Head>
+      <section className='sm:pt-[150px] pt-[100px] pb-[50px] flex flex-col gap-6 custom-container'>
         <div className='w-full'>
           <Image src={'/images/big-placeholder.png'} alt='placeholder' width={500} height={500} className='w-full rounded-[20px]' />
         </div>
         <div className='sm:w-fit w-full rounded-[5px] text-white flex sm:flex-row flex-col sm:items-center items-start sm:gap-4 gap-2'>
           <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main px-4 py-4 rounded-[10px]'>
             <Plane fatherClass={'fill-white rotate-[50deg]'} />
-            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.from")}: <span className='font-semibold'>{tourPackage[replaceWithLocale(router, "from_")]}</span></TextSubtitle>
+            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.from")}: <span className='font-semibold'>{tourPackage.from_uz}</span></TextSubtitle>
           </div>
           <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main px-4 py-4 rounded-[10px]'>
             <Plane fatherClass={'fill-white rotate-[130deg]'} />
-            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.where")}: <span className='font-semibold'>{tourPackage[replaceWithLocale(router, "where_")]}</span></TextSubtitle>
+            <TextSubtitle className={"!text-white capitalize"}>{t("openpackage.where")}: <span className='font-semibold'>{tourPackage.where_uz}</span></TextSubtitle>
           </div>
           <div className='sm:w-fit w-full flex flex-row items-center gap-2 bg-main px-4 py-4 rounded-[10px]'>
             <IconMoney fatherClass={''} />
@@ -80,13 +80,13 @@ const OpenDirection = () => {
           </div>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>{tourPackage[replaceWithLocale(router, "description_")]}</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{tourPackage.descruzion_uz}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.from")}: {tourPackage[replaceWithLocale(router, "from_")]}</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.from")}: {tourPackage.from_uz}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
-          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.where")}: {tourPackage[replaceWithLocale(router, "where_")]}</TextSubtitle>
+          <TextSubtitle className={'text-start !text-black'}>{t("openpackage.where")}: {tourPackage.where_uz}</TextSubtitle>
         </div>
         <div className='bg-main/[0.03] p-4 border rounded-[10px] flex flex-col gap-2'>
           <TextSubtitle className={'text-start !text-black'}>{t("openpackage.days")}: {tourPackage.days
@@ -104,8 +104,7 @@ const OpenDirection = () => {
             <TextSubtitle className={"!text-black capitalize"}>{t('openpackage.manager')}</TextSubtitle>
           </div>
         </Link>
-      </section> */}
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur iusto officiis laborum commodi vero quisquam totam rem quam sapiente nostrum? Mollitia voluptatem quae sint consequuntur nesciunt ut. Sapiente cumque, cupiditate adipisci quas quod ratione pariatur ea, eveniet reprehenderit velit odit accusamus. Labore quod dolorum dignissimos pariatur. Hic, nesciunt nostrum deserunt quasi cupiditate rem vitae quibusdam, autem dicta aliquid nihil ad. Aspernatur laudantium vel nemo harum, a debitis quasi numquam libero inventore dolorem blanditiis hic illo nostrum, consequuntur, quae sint quod dolor quis voluptatibus. Iste illo doloremque est consequuntur vitae quae explicabo architecto? Dolorem alias animi reiciendis quam ipsum quae provident quasi nihil consectetur, accusamus, impedit autem asperiores doloremque praesentium, voluptate atque. Animi eos nobis voluptatem molestiae enim distinctio eaque voluptate perferendis iusto! Culpa iure corrupti hic voluptate aspernatur, quos id sed laborum unde possimus, doloremque, animi laboriosam quo dolor perspiciatis dignissimos illum vero expedita nam quidem eligendi illo? Nam neque deserunt placeat sunt reprehenderit suscipit eligendi et tempore, enim corporis, maiores ullam accusamus delectus ratione quam. Minima ipsa provident deserunt quae, doloremque eveniet cum harum quaerat molestias iste itaque corrupti rem, repudiandae sunt commodi quidem debitis excepturi repellat vel a. Ipsam, tempore nobis quas beatae laboriosam qui? Distinctio, iste eaque?
+      </section>
     </>
   )
 }
